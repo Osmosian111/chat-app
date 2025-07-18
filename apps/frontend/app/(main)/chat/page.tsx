e7dba33fb2a03e68d7af9b4e056bbea0f1d32a48
Osmocian111 <neevjain482@gmail.com>
2025-07-18 22:06:44 +0530

    Now auth is required to visit /chat route

--- apps/frontend/app/(main)/chat/page.tsx
+++ apps/frontend/app/(main)/chat/page.tsx
@@ -1,8 +1,20 @@
 import LeftBlock from "@/component/LeftBlock";
 import RightBlock from "@/component/RightBlock";
 import SpliteScreen from "@/component/SpliteScreen";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Chat = () => {
+import jwt, { JwtHeader, JwtPayload } from "jsonwebtoken";
+
+async function isLoggedIn() {
+  const cookie = (await cookies()).get("chat-app-token")?.value;
+  if (!cookie) redirect("/signin");
+  const decode = jwt.verify(cookie, process.env.JWT_SECRET ?? " ");
+  if (!decode) redirect("/signin");
+}
+
+const Chat = async () => {
+  await isLoggedIn();
   return (
     <>
       <SpliteScreen leftValue="35%" rightValue="65%">
